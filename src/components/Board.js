91c7196107b982cdbadaa9d9f24744949dac7fa0
ComfortPrince <comfort.prince.hluyo@gmail.com>
2024-03-19 15:22:28 +0200

    Added logic for checking the winner.

--- src/components/Board.js
+++ src/components/Board.js
@@ -8,9 +8,8 @@ export default function Board() {
 
 	function handleClick(i) {
 		const nextSquares = squares.slice()
-		
-		console.log(nextSquares)
-		if(nextSquares[i] !== null) return
+
+		if(nextSquares[i] || calculateWinner(squares)) return
 
 		if(xIsNext){
 			nextSquares[i] = 'X'
@@ -41,6 +40,27 @@ export default function Board() {
 			</div>
 		</>
 	);
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 
