73e247d8e9e6fe474316d8f88bb35ebb0a537044
ComfortPrince <comfort.prince.hluyo@gmail.com>
2024-03-19 13:15:51 +0200

    Setup a local environment for the TicTacToe app.

---
+++ src/components/Square.js
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+export default function Square() {
+	const [value, setValue] = useState(null);
+
+	function handleClick() {
+		console.log('clicked')
+	}
+
+  	return <button className="square" onClick={handleClick} >{value}</button>;
+}
