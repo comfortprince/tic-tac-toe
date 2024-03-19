8ce2d8e0209339ec91dfbf1eef205fa2f99ab68b
ComfortPrince <comfort.prince.hluyo@gmail.com>
2024-03-19 14:10:39 +0200

    Lifted each square's state to the board component

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,11 +1,5 @@
 import { useState } from 'react';
 
-export default function Square() {
-	const [value, setValue] = useState(null);
-
-	function handleClick() {
-		console.log('clicked')
-	}
-
-  	return <button className="square" onClick={handleClick} >{value}</button>;
+export default function Square({value}) {
+  	return <button className="square"></button>;
 }
